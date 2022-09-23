0dc6ca2d809c3d5bdb14f36643cd5c43cf6fa2d9
Geoffrey Arner <geoffarner5@gmail.com>
2022-09-23 10:19:46 -0500

    JS Practice

--- practice.js
+++ practice.js
@@ -1,9 +1,12 @@
 //  basic array - select:less than 100
 
 // function less_Than(input) {
-//   output = input.filter((item) => item < 100);
+//   return input.filter((item) => item < 100);
 // }
-// less_Than([99, 101, 88, 4, 2000, 50]);
-// console.log(output);
+// console.log(less_Than([99, 101, 88, 4, 2000, 50]));
 
 // basic array - map double
+// function double_Numbers(input) {
+//   return input.map((num) => num * 2);
+// }
+// console.log(double_Numbers([4, 2, 5, 99, -4]));

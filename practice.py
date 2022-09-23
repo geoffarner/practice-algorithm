a595accde3b28fc4762c4874d434490a7ec28f26
Geoffrey Arner <geoffarner5@gmail.com>
2022-09-23 10:27:41 -0500

    python practice

--- practice.py
+++ practice.py
@@ -3,4 +3,7 @@
 #     return sorted(item for item in x if item < 100)
 # print(less_than([99, 101, 88, 4, 2000, 50]))
 
-# basic array - Map: Double
+# # basic array - Map: Double
+# def double_numbers(input):
+#     return [num * 2 for num in input]
+# print(double_numbers([4, 2, 5, 99, -4]))
